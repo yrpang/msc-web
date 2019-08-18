745da636cb83dfb4b8c31a1f1eac7a1ac5025ccc
yrpang <yrpang@outlook.com>
2019-08-18 21:14:54 +0800

    delete broken js

--- static/js1/plugin.js
+++ static/js1/plugin.js
@@ -6,9 +6,7 @@
 		$(".pic").mouseenter(function(){
 			//$(this).css("background","#000");
 		});	
-		$(".pic").mouseleave()(function(){
-			//$(this).css("background","#f1f1f1");
-		});	
+
 	}
 	
 	//regeister Plugin
