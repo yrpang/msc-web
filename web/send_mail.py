a6575dd291997abb58b55f038ea71df7de43ffdd
yrpang <yrpang@outlook.com>
2019-09-17 22:52:16 +0800

    邮件

--- web/send_mail.py
+++ web/send_mail.py
@@ -14,9 +14,7 @@ html_content = '''
                     <p>    请大家合理安排时间。无法免试的请戳管理！技术部一面可以去多个部门免试，但是最后只能选择一个组。</p>
                 '''
 
-message = ("西电MSC俱乐部一面通知", "周三晚18:30-22:00将在大活522进行技术部：应用数学组、科研组、GAME组、硬件组、ACM组、web组的免试。请大家合理安排时间。无法免试的请戳管理！技术部一面可以去多个部门免试，但是最后只能选择一个组。",
-           str(settings.EMAIL_HOST_USER), ['yrpang@outlook.com'])
+message = ("西电MSC俱乐部一面通知", "周三晚18:30-22:00将在大活522进行技术部：应用数学组、科研组、GAME组、硬件组、ACM组、web组的免试。请大家合理安排时间。无法免试的请戳管理！技术部一面可以去多个部门免试，但是最后只能选择一个组。", str(settings.EMAIL_HOST_USER), ['yrpang@outlook.com'], html_content=html_content)
 
 
 send_mass_mail((message), fail_silently=False)
-
